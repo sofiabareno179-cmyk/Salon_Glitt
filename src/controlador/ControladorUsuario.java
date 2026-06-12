601086fa0fb47d3001e16286507895b7f2b69f7e
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-06-12 17:23:53 -0500

    Update ControladorUsuario.java

--- src/controlador/ControladorUsuario.java
+++ src/controlador/ControladorUsuario.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
-
+import java.awt.event.ActionEvent;
+import modelo.Usuario;
 /**
  *
  * @author LENOVO
  */
 public class ControladorUsuario {
+    
+    public void controlarAccion(ActionEvent evento, Usuario unUsuario){
+        String accion = evento.getActionCommand();
+        switch(accion){
+            case "Insertar":
+                unUsuario.insertar();
+            break;
+            case "Modificar":
+                unUsuario.modificar();
+            break;
+            case "Eliminar":
+                unUsuario.eliminar();
+            break;
+        }
+    }
     
 }
