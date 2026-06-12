86b0d379e0e787bbf49485a5878e5ce3490830b3
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-06-12 17:13:19 -0500

    Update FRMAgenda.java

--- src/vista/FRMAgenda.java
+++ src/vista/FRMAgenda.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
 package vista;
-
+import controlador.ControladorAgenda; 
+import modelo.Agenda;                 
+import javax.swing.table.DefaultTableModel;
+import java.util.Iterator;
 /**
  *
  * @author LENOVO
