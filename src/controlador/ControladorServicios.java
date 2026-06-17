9133ae228cf70861be009e2db391019883f63cfa
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-06-17 17:38:11 -0500

    Update ControladorServicios.java

--- src/controlador/ControladorServicios.java
+++ src/controlador/ControladorServicios.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
-
+import java.awt.event.ActionEvent;
+import modelo.Servicios;
 /**
  *
  * @author LENOVO
  */
 public class ControladorServicios {
     
+    public void controlarAccion(ActionEvent evento, Servicios unServicio){
+        String accion = evento.getActionCommand();
+        switch(accion){
+            case "Insertar":
+                unServicio.insertar();
+            break;
+            case "Modificar":
+                unServicio.modificar();
+            break;
+            case "Eliminar":
+                unServicio.eliminar();
+            break;
+        }
+    }
 }
