14faa75f28f7eb5b741b9874edee825d1f63da47
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-06-28 23:40:03 -0500

    Update FRMBloqueos.java

--- src/vista/FRMBloqueos.java
+++ src/vista/FRMBloqueos.java
@@ -198,9 +198,16 @@ public class FRMBloqueos extends javax.swing.JInternalFrame {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.String.class, java.lang.Integer.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         tblBloqueos.addMouseListener(new java.awt.event.MouseAdapter() {
