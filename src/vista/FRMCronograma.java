a74006cf00beb104b9fbc2e39bb2d088e13181ed
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-06-29 20:44:35 -0500

    Update FRMCronograma.java

--- src/vista/FRMCronograma.java
+++ src/vista/FRMCronograma.java
@@ -30,11 +30,11 @@ public class FRMCronograma extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 591, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 392, Short.MAX_VALUE)
         );
 
         pack();
